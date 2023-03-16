5f0a03784203b5555109a08ee13216acd24c9060
jabir-st-dev <127714535+jabir-st-dev@users.noreply.github.com>
2023-03-16 11:53:43 +0600

    v1.0.2

--- database/url.js
+++ database/url.js
@@ -1,16 +1,25 @@
+const shortUrls = {};
 const urls = {};
 
-exports.add = function ({ original_url , short_url}) {
-    console.log({original_url: urls[short_url]});
-    if(urls[short_url] === undefined){
-        urls[short_url] = original_url;
+exports.add = function ({original_url}) {
+    console.log({original_url: urls[original_url]});
 
-        return { original_url , short_url};
+    if (urls[original_url] === undefined) {
+        let short_url = Math.floor(Math.random() * 100000).toString();
+
+        while (shortUrls[short_url] !== undefined) {
+            short_url = Math.floor(Math.random() * 100000).toString();
+        }
+
+        shortUrls[short_url] = original_url;
+        urls[original_url] = short_url;
+
+        return {original_url, short_url};
+    } else {
+        return {original_url, short_url: urls[original_url]}
     }
-
-    return { original_url , short_url};
 };
 
 exports.get = function ({short_url}) {
-    return urls[short_url];
+    return shortUrls[short_url];
 }
--- routes/urlshortener.js
+++ routes/urlshortener.js
@@ -5,40 +5,27 @@ const router = express.Router();
 const url_database = require('../database/url');
 
 router.post('/', function (req, res) {
-    const original_url = req.body.url;
+    const urlObject = new URL(req.body.url);
 
-    console.log(original_url);
+    const original_url = urlObject.origin;
 
-    const isValidUrl = urlString=> {
-        const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-            '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-        return !!urlPattern.test(urlString);
-    }
+    console.log({original_url})
 
-    if(isValidUrl(original_url)) {
-        const domain = original_url.split('www.')[1].split('/')[0];
-
-        console.log({domain});
-        const short_url = Math.floor(Math.random() * 100000).toString();
-
-        const url = url_database.add({ original_url , short_url});
-
-        console.log({url});
-
-        return res.json(url);
-    } else {
-        return res.json({ error: 'invalid url' });
-    }
+    dns.lookup(urlObject.hostname, (err, address, family) => {
+        if (err) {
+            res.json({error: 'invalid url'});
+        } else {
+            const url = url_database.add({original_url});
+            console.log({url});
+            return res.json(url);
+        }
+    });
 });
 
 router.get('/:short_url', function (req, res) {
     const short_url = req.params.short_url;
     const url = url_database.get({short_url})
     res.redirect(url)
-})
+});
 
 module.exports = router;
