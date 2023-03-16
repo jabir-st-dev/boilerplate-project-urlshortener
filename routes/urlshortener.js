3923838af8ef4dc42d6cf361a995a7b11b256e57
jabir-st-dev <127714535+jabir-st-dev@users.noreply.github.com>
2023-03-16 13:59:39 +0600

    database introduced
    @databases/sqlite
    It is a memory database.

--- routes/urlshortener.js
+++ routes/urlshortener.js
@@ -2,29 +2,31 @@ const express = require('express');
 const dns = require("dns");
 const router = express.Router();
 
-const url_database = require('../database/url');
+const url_database = require('../database/');
 
 router.post('/', function (req, res) {
     const urlObject = new URL(req.body.url);
 
     const original_url = urlObject.origin;
 
-    console.log({original_url})
+    // console.log({original_url})
 
-    dns.lookup(urlObject.hostname, (err, address, family) => {
+    dns.lookup(urlObject.hostname, async function (err, address, family) {
         if (err) {
             res.json({error: 'invalid url'});
         } else {
-            const url = url_database.add({original_url});
-            console.log({url});
+            const url = await url_database.add({original_url});
+
+            // console.log({url});
             return res.json(url);
         }
     });
 });
 
-router.get('/:short_url', function (req, res) {
+router.get('/:short_url', async function (req, res) {
     const short_url = req.params.short_url;
-    const url = url_database.get({short_url})
+    const url = await url_database.get({short_url})
+    // console.log({url})
     res.redirect(url)
 });
 
